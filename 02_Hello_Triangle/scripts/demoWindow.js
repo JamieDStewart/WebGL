1377ca460f2475dfe6f185fd6aa421691e152570
JamieDStewart <stewart.jamie.d@gmail.com>
2019-02-14 11:13:30 +0000

    Using an interleaved vertex structure

--- 02_Hello_Triangle/scripts/demoWindow.js
+++ 02_Hello_Triangle/scripts/demoWindow.js
@@ -20,25 +20,70 @@ var FS_Source =
     'fragColour = vColour;\n' +
     '}\n';
 
+class vec4{
+    /**
+     * 
+     * @param {number} a_x 
+     * @param {number} a_y 
+     * @param {number} a_z 
+     * @param {number} a_w 
+     */
+    constructor(a_x, a_y, a_z, a_w){
+        this.x = a_x;
+        this.y = a_y;
+        this.z = a_z;
+        this.w = a_w;
+    }
+}
+
+
 class Vertex{
-    constructor(){
-        this.position;
-        this.colour;
+    /**
+     * 
+     * @param {vec4} a_pos 
+     * @param {vec4} a_col 
+     */
+    constructor(a_pos, a_col){
+        this._position = a_pos;
+        this._colour = a_col;
     }
 
     get position(){
-        return this.position;
+        return this._position;
     }
     set position(newPosition){
-        this.position = newPosition;
+        this._position = newPosition;
     }
 
     get colour(){
-        return this.colour;
+        return this._colour;
     }
     set colour(newColour){
-        this.colour = newColour;
+        this._colour = newColour;
     }
+    toArray(){
+        var arr = [];
+        for( var v in this){
+           for( var i in this[v]){
+                arr.push(this[v][i]);
+            }
+           
+        }
+        return arr;
+    }
+}
+
+function VertexDataAsRawArray(parameter) {
+    var rawdata = [];
+    var vt = null;
+    var arr = null;
+    for (var i = 0; i < parameter.length; ++i) { // loop through properties
+        vt = parameter[i];
+        arr = vt.toArray();
+        rawdata = rawdata.concat(arr);
+    }
+    return rawdata;
+
 }
 
 //Create the main entry point for this application
@@ -63,16 +108,22 @@ function main() {
    
     //The shaders have been loaded and linked into a shader program
     //create a vertex structure and fill it with the appropriate data
-    var vertex_data = [-0.5,-0.75, 0, 1.0, 1.0, 0.0, 0.0, 1.0,
-        0.5,-0.75, 0, 1.0, 0.0, 1.0, 0.0, 1.0,
-        0  , 0.25, 0, 1.0, 0.0, 0.0, 1.0, 1.0];
+    var vertex_structure = [ 
+        new Vertex( new vec4(-0.5, -0.75, 0, 1.0), new vec4(1.0 ,0   ,0   , 1.0)),
+        new Vertex( new vec4( 0.5, -0.75, 0, 1.0), new vec4(0   ,1.0 ,0   , 1.0)),
+        new Vertex( new vec4( 0  ,  0.25, 0, 1.0), new vec4(0   ,0   ,1.0 , 1.0))];
+    //WebGL requires the data to be sent as an array of Float32 objects this function 
+    //transforms the vertex structure into that float array to send to the GL buffer
+    var value = VertexDataAsRawArray(vertex_structure);
     
-        var indices = [ 0, 1, 2];
+    
+    var indices = [ 0, 1, 2];
     
     var vbo = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertex_data),  gl.STATIC_DRAW);
-   
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(value),  gl.STATIC_DRAW);
+    delete value;
+    
     var ibo = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
@@ -83,7 +134,7 @@ function main() {
    
     gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
-    
+
     gl.enableVertexAttribArray(posLoc);
     gl.enableVertexAttribArray(colLoc);
 
